f38cb1c497135393900505016d3d9ead763309ae
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-06-06 19:01:50 -0500

    Added Header and ApplicationViews components

---
+++ client/src/components/ApplicationViews.js
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
+import Login from "./Login";
+import Register from "./Register";
+
+// Game Components
+
+export default function ApplicationViews() {
+    const { isLoggedIn } = useContext(UserProfileContext);
+
+    return (
+        <main>
+            <Switch>
+                {/* -----------------LOGIN & REGISTRATION PAGES---------------- */}
+                <Route path="/" exact>
+                    {isLoggedIn ? <Redirect to="/"/> : <Redirect to="/login" />}
+                </Route>
+
+                {/* Login Page */}
+                <Route path="/login">
+                    <Login />
+                </Route>
+
+                {/* Registration Page */}
+                <Route path="/register">
+                    <Register />
+                </Route>
+
+                {/* ----------------------- GAME ----------------------- */}
+
+            </Switch>
+        </main>
+    );
+};
